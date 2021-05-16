8792f27947a52b9188436a26b3e4fcfe0c78d27b
Rupesh-King <76781046+Rupesh-King@users.noreply.github.com>
2021-05-16 12:07:33 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,13 +15,13 @@ var player1score =0;
 var player2score =0;
 
 function preload(){
-  back_img = loadImage("New folder/jungle.jpg");
-  player_img = loadImage("New folder/basket2.png");
-  fruit1_img = loadImage("New folder/apple2.png");
-  fruit2_img = loadImage("New folder/banana2.png");
-  fruit3_img = loadImage("New folder/melon2.png");
-  fruit4_img = loadImage("New folder/orange2.png");
-  fruit5_img = loadImage("New folder/pineapple2.png");
+  back_img = loadImage("New folder/jungle-Copy.jpg");
+  player_img = loadImage("New folder/basket2-Copy.png");
+  fruit1_img = loadImage("New folder/apple2-Copy.png");
+  fruit2_img = loadImage("New folder/banana2-Copy.png");
+  fruit3_img = loadImage("New folder/melon2-Copy.png");
+  fruit4_img = loadImage("New folder/orange2-Copy.png");
+  fruit5_img = loadImage("New folder/pineapple2-Copy.png");
   fruitGroup = new Group();
 }
 function setup() {
